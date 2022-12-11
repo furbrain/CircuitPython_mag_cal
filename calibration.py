5bd4af1ddade1904abdd09622c627dbfc9a9e7af
Phil Underwood <beardydoc@gmail.com>
2022-12-11 22:44:46 +0000

    implement least squares non-linear fit

--- calibration.py
+++ calibration.py
@@ -29,11 +29,15 @@ There are several axis conventions used.
   facing away from you and Z is up.
 
 """
+import json
+
+import nm
+from rbf import RBF
 from sensor import Sensor
-from utils import normalise
+from utils import normalise, solve_least_squares
 
 try:
-    from typing import Tuple
+    from typing import Tuple, Dict
 except ImportError:
     # ignore if running in CircuitPython/MicroPython
     pass
@@ -60,6 +64,10 @@ class Calibration:
     Object representing a magnetometer and accelerometer calibration
     """
 
+    MAGNETOMETER = 1
+    ACCELEROMETER = 2
+    BOTH = MAGNETOMETER | ACCELEROMETER
+
     def __init__(self, mag_axes: str = "+X+Y+Z", grav_axes: str = None):
         """
         Create an object representing the calibration coefficients for a combined
@@ -80,22 +88,54 @@ class Calibration:
         self.grav = Sensor(axes=grav_axes)
         self.ready = False
 
-    def to_json(self) -> str:
+    def as_dict(self) -> Dict:
+        """
+        Convert the current calibration to a dictionary
+
+        :return: The calibration as a dictionary
+        :rtype: dict
+        """
+        dct = {
+            "mag": self.mag.as_dict(),
+            "grav": self.grav.as_dict(),
+            "ready": self.ready,
+        }
+        return dct
+
+    def as_json(self) -> str:
         """
         Report current calibration as JSON text
 
         :return: The calibration in JSON format
         :rtype: str
         """
+        return json.dumps(self.as_dict())
 
-    # def from_json(self, text: str) -> None:
-    #     """
-    #     Read some JSON text previosuly produced by _`to_json`
-    #
-    #     :param text: JSON text to read
-    #     :return: None
-    #     """
-    #
+    @classmethod
+    def from_dict(cls, dct: Dict) -> "Calibration":
+        """
+        Create a Calibration object based on a dict previously produced by _`as_dict`
+
+        :param dict dct: dict to instantiate
+        :return: Calibration object
+        """
+        instance = cls()
+        instance.mag = Sensor.from_dict(dct["mag"])
+        instance.grav = Sensor.from_dict(dct["grav"])
+        instance.ready = dct["ready"]
+        return instance
+
+    @classmethod
+    def from_json(cls, text: str) -> "Calibration":
+        """
+        Create a Calibration object based on some JSON text previosuly produced by _`as_json`
+
+        :param str text: JSON text to read
+        :return: Calibration object
+        """
+        dct = json.loads(text)
+        return cls.from_dict(dct)
+
     def fit_ellipsoid(
         self, mag_data: np.ndarray, grav_data: np.ndarray
     ) -> Tuple[float, float]:
@@ -110,9 +150,10 @@ class Calibration:
         """
         mag_accuracy = self.mag.fit_ellipsoid(mag_data)
         grav_accuracy = self.grav.fit_ellipsoid(grav_data)
+        self.ready = True
         return mag_accuracy, grav_accuracy
 
-    def accuracy(self, data):
+    def accuracy(self, data) -> float:
         """
         Calculate average accuracy for a set of multiple readings taken
 
@@ -121,7 +162,7 @@ class Calibration:
           ``grav_data1`` is a (N,3) numpy array of readings around the axis in the first
           direction, and ``mag_data2`` and ``grav_data2`` is a (M,3) numpy array of readings around
           the specified axis in another direction.
-        :return:
+        :return: Average standard deviation of readings in degrees
         """
         results = 0
         for mag, grav in data:
@@ -130,34 +171,173 @@ class Calibration:
             results += np.linalg.norm(stds)
         return np.degrees(results / len(data))
 
+    def uniformity(self, mag_data, grav_data):
+        """
+        Check the uniformity of the data - how well the calibrated data points fit on
+        a sphere of radius 1.0
+
+        :param np.ndarray mag_data: Numpy array of magnetic readings of shape (N,3)
+        :param np.ndarray grav_data: Numpy array of gravity readings of shape (M,3)
+        :return: (mag_accuracy, grav_accuracy) How well the calibrated model fits the data.
+          Lower numbers are better
+        """
+        return self.mag.uniformity(mag_data), self.grav.uniformity(grav_data)
+
     def align_along_axis(self, data, axis="Y") -> float:
         """
         Take multiple reading with the device pointing in the same direction, but rotated around
         ``axis``. You can repeat this with several different directions. This function will take
-        this data and ensure that your sensors aligned relative to each other. It will also apply a
-        non-linear correction
+        this data and ensure that your sensors aligned relative to each other. This function
+        requires that you have run `Calibration.fit_ellipsoid` beforehand.
 
         :param data: A list of paired magnetic and gravity readings e.g.:
-          ``[(mag_data1, grav_data1), (mag_data2, grav_data2)]``, where ``mag_data1`` and
-          ``grav_data1`` is a (N,3) numpy array of readings around the axis in the first
-          direction, and ``mag_data2`` and ``grav_data2`` is a (M,3) numpy array of readings around
+          ``[(mag_data1, grav_data1), (mag_data2, grav_data2), ...]``, where ``mag_data1`` and
+          ``grav_data1`` are (N,3) numpy arrays of readings around the axis in the first
+          direction, and ``mag_data2`` and ``grav_data2`` are (M,3) numpy arrays of readings around
           the specified axis in another direction.
         :param axis: Axis you have rotated your device around. Defaults to ``"Y"``
-        :param int non_linear: Set to non-zero if you want to use a non-linear correction. This
-          allows you to compensate for devices which do not have a linear response between the
-          magnetic or gravity field and their output. It is recommended to use either 1,3 or 5 for
-          this function; odd numbers generally get better results, but there is a significant risk
-          of overfitting if higher numbers are used.
-          See `Underwood, Phil (2021) Non-linear Calibration of a Digital Compass and
-          Accelerometer, Cave Radio Electronics Group Journal 114, pp7-10. June 2021
-          <https://github.com/furbrain/SAP5/blob/master/doc/non-linear_calibration.pdf>`_
-          for more details on the algorithm used.
-        :return: Standard deviation of accuracy of calibration in degrees
+        :return: Average standard deviation of readings in degrees, after calibration
         """
         mag_data = [x[0] for x in data]
         grav_data = [x[1] for x in data]
         self.mag.align_along_axis(mag_data, axis)
         self.grav.align_along_axis(grav_data, axis)
+        return self.accuracy(data)
+
+    def apply_non_linear_correction(
+        self, data, axis: str = "Y", param_count: int = 3, sensor: int = MAGNETOMETER
+    ):
+        """
+        Compensate for devices which do not have a linear response between the
+        magnetic or gravity field and their output. It is recommended to use a
+        ``param_count`` of either 1, 3 or 5 for this function; odd numbers generally get better
+        results, but there is a significant risk of overfitting if higher numbers are used.
+        See `Underwood, Phil (2021) Non-linear Calibration of a Digital Compass and
+        Accelerometer, Cave Radio Electronics Group Journal 114, pp7-10. June 2021
+        <https://github.com/furbrain/SAP5/blob/master/doc/non-linear_calibration.pdf>`_
+        for more details on the algorithm used.
+
+        This function requires that you have run both `Calibration.fit_ellipsoid` and
+        `Calibration.align_along_axis` beforehand.
+
+        :param data: A list of paired magnetic and gravity readings e.g.:
+          ``[(mag_data1, grav_data1), (mag_data2, grav_data2), ...]``, where ``mag_data1`` and
+          ``grav_data1`` is a (N,3) numpy array of readings around the axis in the first
+          direction, and ``mag_data2`` and ``grav_data2`` is a (M,3) numpy array of readings around
+          the specified axis in another direction.
+        :param str axis: Axis along which the device has been rotated for the given readings.
+        :param int param_count: Number of parameters to use per sensor axis. Larger numbers
+          will take substantially more time to calculate. Default is 3
+        :param sensor: Whether to calibrate the magnetometer, accelerometer or both. Must be one
+          of `Calibration.MAGNETOMETER`, `Calibration.ACCELEROMETER`, or `Calibration.BOTH`.
+          Default is ``MAGNETOMETER``, as accelerometers are generally quite well-behaved.
+        :return: Standard deviation of accuracy of calibration in degrees
+        """
+        params_per_sensor = param_count * 2
+        if sensor == self.BOTH:
+            params_total = params_per_sensor * 2
+        else:
+            params_total = params_per_sensor
+        all_mag = np.concatenate([x[0] for x in data])
+        all_grav = np.concatenate([x[1] for x in data])
+        axis_index = "XYZ".index(axis.upper())  # which axis by number...
+
+        # this is the minimisation function we wish to calculate...
+        def min_func(x):
+            if sensor & self.MAGNETOMETER:
+                params = x[:params_per_sensor]
+                # do not optimize axis around which we are rotating - set these coefficients to zero
+                params = np.insert(params, axis_index * 3, [0, 0, 0])
+                self.mag.set_non_linear_params(params)
+            if sensor & self.ACCELEROMETER:
+                params = x[-params_per_sensor:]
+                # do not optimize axis around which we are rotating - set these coefficients to zero
+                params = np.insert(params, axis_index * 3, [0, 0, 0])
+                self.grav.set_non_linear_params(params)
+            return self.accuracy(data) + sum(self.uniformity(all_mag, all_grav))
+
+        x_initial = np.full(params_total, 0.0)
+        minimizer = nm.Minimizer(min_func, x_initial, adaptive=False)
+        results = minimizer.optimize()
+        if results["status"] != "success":
+            raise RuntimeError("Unable to calibrate - optimisation routine failed")
+        min_func(results["x"])
+        return results["iterations"], self.accuracy(data)
+
+    def apply_non_linear_correction2(self, data, param_count: int = 3):
+        """
+        Compensate for devices which do not have a linear response between the
+        magnetic or gravity field and their output. It is recommended to use a
+        ``param_count`` of either 1, 3 or 5 for this function; odd numbers generally get better
+        results, but there is a significant risk of overfitting if higher numbers are used.
+        See `Underwood, Phil (2021) Non-linear Calibration of a Digital Compass and
+        Accelerometer, Cave Radio Electronics Group Journal 114, pp7-10. June 2021
+        <https://github.com/furbrain/SAP5/blob/master/doc/non-linear_calibration.pdf>`_
+        for more details on the algorithm used.
+
+        This function requires that you have run both `Calibration.fit_ellipsoid` and
+        `Calibration.align_along_axis` beforehand.
+
+        :param data: A list of paired magnetic and gravity readings e.g.:
+          ``[(mag_data1, grav_data1), (mag_data2, grav_data2), ...]``, where ``mag_data1`` and
+          ``grav_data1`` is a (N,3) numpy array of readings around the axis in the first
+          direction, and ``mag_data2`` and ``grav_data2`` is a (M,3) numpy array of readings around
+          the specified axis in another direction.
+        :param int param_count: Number of parameters to use per sensor axis. Larger numbers
+          will take substantially more time to calculate. Default is 3
+        :return: Standard deviation of accuracy of calibration in degrees
+        """
+        self.mag.set_linear()
+        expected_mags = []
+        raw_mags = []
+        for mag, grav in data:
+            expected, raw = self._get_raw_and_expected_mag_data(grav, mag)
+            expected_mags.extend(expected)
+            raw_mags.extend(raw)
+        # create least squares set of sums
+        params = self._get_lstsq_non_linear_params(param_count, expected_mags, raw_mags)
+        all_params = np.zeros(param_count * 3)
+        all_params[:param_count] = params[:param_count]
+        all_params[-param_count] = params[-param_count]
+        self.mag.set_non_linear_params(params)
+        return self.accuracy(data)
+
+    @staticmethod
+    def _get_lstsq_non_linear_params(param_count, expected_mags, raw_mags):
+        rbf = RBF(np.zeros(param_count))
+        input_data = np.zeros((0, param_count * 2))
+        output_data = np.zeros((0,))
+        for raw, expected in zip(raw_mags, expected_mags):
+            diff = expected - raw
+            factors = rbf.get_gaussians(raw).transpose()
+            temp_input = np.zeros((2, param_count * 2))
+            temp_input[0, :param_count] = factors[0]
+            temp_input[1, -param_count:] = factors[2]
+            input_data = np.concatenate((input_data, temp_input))
+            output_data = np.concatenate((output_data, [diff[0], diff[2]]))
+        params = solve_least_squares(input_data, output_data)
+        return params
+
+    def _get_raw_and_expected_mag_data(self, grav, mag):
+        # pylint: disable=invalid-name,too-many-locals
+        rotated_mags = []
+        rot_mats = []
+        expected_mags = []
+        raw_mags = []
+        for m, g in zip(mag, grav):
+            _, _, roll = self.get_angles(m, g)
+            c = np.cos(np.radians(roll))
+            s = np.sin(np.radians(roll))
+            rot_mat = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+            raw_mag = self.mag.apply(m)
+            rotated_mag = np.dot(raw_mag, rot_mat.T)
+            raw_mags.append(raw_mag)
+            rotated_mags.append(rotated_mag)
+            rot_mats.append(rot_mat)
+        average_vector = np.mean(rotated_mags, axis=0)
+        for rot_mat in rot_mats:
+            expected_mags.append(np.dot(average_vector, rot_mat))
+        return expected_mags, raw_mags
 
     def get_orientation_vector(self, mag, grav):
         """
@@ -170,7 +350,7 @@ class Calibration:
         :rtype: Numpy array of same shape as mag, with device orientation.
         """
         orientation = self.get_orientation_matrix(mag, grav)
-        return orientation[1]
+        return orientation[..., :, 1]
 
     def get_orientation_matrix(self, mag, grav) -> np.ndarray:
         """
@@ -187,23 +367,73 @@ class Calibration:
         upward = normalise(self.grav.apply(grav)) * -1
         east = normalise(np.cross(mag, upward))
         north = normalise(np.cross(upward, east))
-        orientation = np.array((east, north, upward)).transpose()
+        orientation = np.array((east, north, upward))
+        if len(orientation.shape) == 3:
+            orientation = orientation.transpose((1, 0, 2))
         return orientation
 
-    def get_angles(self, mag, grav) -> Tuple[np.ndarray, np.ndarray]:
+    def get_angles(self, mag, grav) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         Get device azimuth(bearing) and inclination, given the magnetic and gravity readings
 
         :param np.ndarray mag: Magnetic readings, either as numpy array or sequence of 3 floats
         :param np.ndarray grav: Gravity readings, either as numpy array or sequence of 3 floats
-        :return: (azimuth, inclination) in degrees
+        :return: (azimuth, inclination, roll) in degrees
         """
-        orientation = self.get_orientation_vector(mag, grav)
-        azimuth = np.degrees(np.arctan2(orientation[..., 0], orientation[..., 1]))
-        inclination = np.degrees(
-            np.arctan2(
-                orientation[..., 2], np.linalg.norm(orientation[..., 0:2], axis=-1)
-            )
+
+        matrix = self.get_orientation_matrix(mag, grav)
+        azimuth, inclination, roll = self.matrix_to_angles(matrix)
+        return azimuth, inclination, roll
+
+    @staticmethod
+    def matrix_to_angles(matrix: np.ndarray):
+        """
+        Extract the rotation angles from a matrix. Angles are "zxy" rotations (azimuth, pitch, roll)
+
+        :param np.ndarray matrix:
+        :return: azimuth, pitch, roll
+        """
+        theta1 = np.arctan2(matrix[..., 0, 1], matrix[..., 1, 1])
+        theta2 = np.arctan2(matrix[..., 2, 1] * np.cos(theta1), matrix[..., 1, 1])
+        theta3 = np.arctan2(-matrix[..., 2, 0], matrix[..., 2, 2])
+        azimuth = np.degrees(theta1) % 360
+        inclination = (
+            (np.degrees(theta2) + 90) % 180
+        ) - 90  # force to be in range -90,+90
+        roll = np.degrees(theta3)
+        return azimuth, inclination, roll
+
+    @staticmethod
+    def angles_to_matrix(
+        azimuth: np.ndarray, inclination: np.ndarray, roll: np.ndarray
+    ):
+        # pylint: disable=invalid-name
+        """
+        Create a rotation matrix from angles "zxy" i.e azimmuth, inclination, roll. This is the
+        invers of matrix_to_angles
+
+        :param azimuth: float or np.array of floats for azimuth
+        :param inclination: pitch(rotation around x axis)
+        :param roll: roll around y axis
+        :return:
+        """
+        theta1 = np.radians(-azimuth)
+        theta2 = np.radians(inclination)
+        theta3 = np.radians(roll)
+        c1 = np.cos(theta1)
+        s1 = np.sin(theta1)
+        c2 = np.cos(theta2)
+        s2 = np.sin(theta2)
+        c3 = np.cos(theta3)
+        s3 = np.sin(theta3)
+
+        matrix = np.array(
+            [
+                [c1 * c3 - s1 * s2 * s3, -c2 * s1, c1 * s3 + c3 * s1 * s2],
+                [c3 * s1 + c1 * s2 * s3, c1 * c2, s1 * s3 - c1 * c3 * s2],
+                [-c2 * s3, s2, c2 * c3],
+            ]
         )
-        azimuth %= 360
-        return azimuth, inclination
+        if len(matrix.shape) > 2:
+            matrix = np.moveaxis(matrix, -1, 0)
+        return matrix
--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -1,8 +1,10 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022 Phil Underwood for Underwood Underground
 #
 # SPDX-License-Identifier: MIT
+import itertools
 import json
 import math
+import unittest
 from pathlib import Path
 from unittest import TestCase
 
@@ -13,6 +15,7 @@ from calibration import Calibration
 
 class TestCalibration(TestCase):
     def setUp(self) -> None:
+        np.set_printoptions(precision=4, suppress=True)
         self.fixtures = {}
         for fname in (Path(__file__).parent / "fixtures" / "cal_data").glob("*.json"):
             with open(fname) as fixture:
@@ -23,27 +26,101 @@ class TestCalibration(TestCase):
                 mag = np.reshape(mag, (-1, 3))
                 grav = [x for x in grav if not math.isnan(x)]
                 grav = np.reshape(grav, (-1, 3))
-                self.fixtures[fname] = (mag, grav)
+                mag_aligned = [mag[8:16], mag[16:24]]
+                grav_aligned = [grav[8:16], grav[16:24]]
+                aligned_data = list(zip(mag_aligned, grav_aligned))
+                self.fixtures[fname] = (mag, grav, aligned_data)
 
     def test_fit_ellipsoid(self):
-        for mag, grav in self.fixtures.values():
+        for mag, grav, _ in self.fixtures.values():
             calib = Calibration()
             mag_accuracy, grav_accuracy = calib.fit_ellipsoid(mag, grav)
             self.assertGreater(0.01, mag_accuracy)
             self.assertGreater(0.001, grav_accuracy)
 
     def test_align_along_axis(self):
-        for mag, grav in self.fixtures.values():
+        for mag, grav, aligned_data in self.fixtures.values():
             calib = Calibration()
             calib.fit_ellipsoid(mag, grav)
-            mag_aligned = [mag[8:16], mag[16:24]]
-            grav_aligned = [grav[8:16], grav[16:24]]
-            data = list(zip(mag_aligned, grav_aligned))
-            before = calib.accuracy(data)
-            calib.align_along_axis(data, axis="Y")
-            after = calib.accuracy(data)
+            before = calib.accuracy(aligned_data)
+            calib.align_along_axis(aligned_data, axis="Y")
+            after = calib.accuracy(aligned_data)
             print(before, after)
             # we should expect accuracy to at least double after alignment
             self.assertLess(2.0, before / after)
-            print(calib.get_angles(mag[8:16], grav[8:16]))
-            print(calib.get_angles(mag[16:24], grav[16:24]))
+
+    def test_non_linear(self):
+        for mag, grav, aligned_data in self.fixtures.values():
+            calib = Calibration()
+            calib.fit_ellipsoid(mag, grav)
+            calib.accuracy(aligned_data)
+            axis = calib.align_along_axis(aligned_data, axis="Y")
+            _, non_linear = calib.apply_non_linear_correction(
+                aligned_data, sensor=Calibration.MAGNETOMETER, param_count=3
+            )
+            print(non_linear, axis)
+            self.assertLess(
+                non_linear, axis
+            )  # check non_linear process improves accuracy!
+
+    def test_non_linear2(self):
+        for mag, grav, aligned_data in self.fixtures.values():
+            calib = Calibration()
+            calib.fit_ellipsoid(mag, grav)
+            calib.accuracy(aligned_data)
+            axis = calib.align_along_axis(aligned_data, axis="Y")
+            non_linear = calib.apply_non_linear_correction2(aligned_data, param_count=3)
+            print(non_linear, axis)
+            # self.assertLess(
+            #    non_linear, axis
+            # )  # check non_linear process improves accuracy!
+
+    # we skip this as it is more of a debugging tool than true test
+    @unittest.skip
+    def test_display_non_linear_maps(self):
+        # pylint: disable=invalid-name,import-outside-toplevel,cell-var-from-loop
+        import matplotlib.pyplot as plt
+
+        mag, grav, aligned_data = list(self.fixtures.values())[3]
+        calib = Calibration()
+        calib.fit_ellipsoid(mag, grav)
+        calib.accuracy(aligned_data)
+        calib.align_along_axis(aligned_data, axis="Y")
+        xs = np.linspace(-0.1, 0.1, 30)
+        X, Y = np.meshgrid(xs, xs)
+        _, axs = plt.subplots(5, 5)
+        for i, j in itertools.combinations(range(6), 2):
+
+            def func(x, y):
+                params = np.zeros(6)
+                params[i] = x
+                params[j] = y
+                params = np.insert(params, 3, [0, 0, 0])
+                calib.mag.set_non_linear_params(params)
+                return calib.accuracy(aligned_data) + sum(calib.uniformity(mag, grav))
+
+            f = np.vectorize(func)
+            Z = f(X, Y)
+            axs[i, j - 1].pcolormesh(X, Y, Z)
+        plt.show()
+
+    @staticmethod
+    def test_angles_to_matrix_singles():
+        angles = [35, +20, 10]
+
+        matrix = Calibration.angles_to_matrix(*angles)
+        new_angles = Calibration.matrix_to_angles(matrix)
+        np.testing.assert_array_almost_equal(angles, new_angles)
+
+    @staticmethod
+    def test_angles_to_matrix_multiple():
+        azimuths = np.random.uniform(0, 360, 20)
+        inclinations = np.random.uniform(-90, 90, 20)
+        rolls = np.random.uniform(-180, 180, 20)
+
+        matrices = Calibration.angles_to_matrix(azimuths, inclinations, rolls)
+        new_az, new_inc, new_roll = Calibration.matrix_to_angles(matrices)
+
+        np.testing.assert_array_almost_equal(azimuths, new_az)
+        np.testing.assert_array_almost_equal(inclinations, new_inc)
+        np.testing.assert_array_almost_equal(rolls, new_roll)
